a5338ede124aa7bf487f0302575b0896c6aceebd
Pedro Fernandes <fernandespedrobc@gmail.com>
2022-11-02 23:23:05 -0300

    Header initial implementing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { LoginBoxComponent } from './components/login-box/login-box.component';
 import { ContinueButtonComponent } from './components/button/continue-button/continue-button.component';
 import { ButtonSignInComponent } from './components/button/button-sign-in/button-sign-in.component';
+import { NavbarComponent } from './pages/navbar/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginBoxComponent,
     ContinueButtonComponent,
-    ButtonSignInComponent
+    ButtonSignInComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
