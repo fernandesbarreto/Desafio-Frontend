1e0c10f69293f8aea2b8b9f3c7ebe0aaff18da19
Pedro Fernandes <fernandespedrobc@gmail.com>
2022-11-05 17:59:10 -0300

    Hide/Show login

--- src/app/components/login-box/login-box.component.ts
+++ src/app/components/login-box/login-box.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-box.component.css']
 })
 export class LoginBoxComponent implements OnInit {
+  show: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  showHidePass(){
+    this.show = !this.show;
+  }
 }
